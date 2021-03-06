efadb8346e90644dfb4e38e03c4483a006c66283
anthemwingate <49780762+anthemwingate@users.noreply.github.com>
2021-03-06 00:09:51 -0500

    Update finalProject_sttTinker_testRunner.java

--- finalProject_sttTinker_testRunner.java
+++ finalProject_sttTinker_testRunner.java
@@ -1,16 +1,17 @@
 package Assignments.finalProject_WastsonImplementation_SpeechToText;
 
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class finalProject_sttTinker_testRunner {
-	public static Logger Log = Logger.getLogger(finalProject_sttTinker_testRunner.class.toString());
+	public static final Logger LOG = LoggerFactory.getLogger(finalProject_sttTinker_testRunner.class);
 
 	public static void main(String[] args) {
 		try {
 			sttTinker.getText(finalProject_sstTinker_constants.STT_TEST_FILE); // get transcript from audio file
 			//streamingStt_Tinker.getText(); //get transcript from stream
 		} catch(Exception e) {
-			Log.info(e.getClass().getName() + e.getMessage());
+			LOG.info(e.getClass().getName() + e.getMessage());
 			sttTinker.printException(e);
 		}
 	}
